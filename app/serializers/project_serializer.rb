bf4ec867c1ab6be0e30a01ee2bba72782fd38188
kevinsoft108 <kevinsoft108@gmail.com>
2023-01-29 20:54:11 +0700

    Add rspec

--- app/serializers/project_serializer.rb
+++
@@ -1,9 +0,0 @@
-class ProjectSerializer
-  include JSONAPI::Serializer
-
-  attributes :title, :assignee, :status
-
-  attribute :created_at do |project|
-    project.created_at && project.created_at.strftime('%Y-%m-%d')
-  end
-end
